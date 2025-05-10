67012a4061880fe1573d61e8338c826025bb79fe
lowzijian <jalow@moneylion.com>
2025-05-10 22:23:57 +0800

    refactor: declare transaction type typing

--- models/transaction.model.ts
+++ models/transaction.model.ts
@@ -1,10 +1,12 @@
 import { TRANSACTION_TYPE } from "@/constants/transaction";
 import { ValueOf } from "@/utils/types";
 
+export type TransactionType = ValueOf<typeof TRANSACTION_TYPE>;
+
 export interface Transaction {
-    id: string;
-    amount: number;
-    date: string;
-    description: string;
-    type: ValueOf<typeof TRANSACTION_TYPE>;
+  id: string;
+  amount: number;
+  date: string;
+  description: string;
+  type: TransactionType;
 }
