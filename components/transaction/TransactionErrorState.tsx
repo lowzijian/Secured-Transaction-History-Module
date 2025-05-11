7b5b28872d04d1b234438bd625d2efd68aad5e2e
lowzijian <jalow@moneylion.com>
2025-05-11 14:26:46 +0800

    feat: create error state

---
+++ components/transaction/TransactionErrorState.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const TransactionErrorState = () => {
+  return (
+    <View>
+      <Text>Something went wrong</Text>
+      <Text>Please try again later</Text>
+    </View>
+  );
+};
+
+export default TransactionErrorState;
