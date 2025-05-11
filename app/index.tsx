293a69ba831c6967ff6142da88d31f2141fedb3a
lowzijian <jalow@moneylion.com>
2025-05-11 14:27:27 +0800

    feat: integrate transaction histories query hook with loading and empty state

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,28 @@
 import ListRefreshControl from "@/components/ListRefreshControl";
+import TransactionErrorState from "@/components/transaction/TransactionErrorState";
 import TransactionList from "@/components/transaction/TransactionList";
+import TransactionLoadingSkeleton from "@/components/transaction/TransactionLoadingSkeleton";
 import { COLORS, FONT_WEIGHT } from "@/constants/theme";
-import { mockTransactions } from "@/mocks/transaction.mock";
+import useTransactionHistoriesQuery from "@/hooks/useTransactionHistoriesQuery";
 import { Stack } from "expo-router";
 import { ScrollView } from "react-native";
 
 const TransactionHistory = () => {
+  const { data, isLoading, isError } = useTransactionHistoriesQuery();
+
   // TODO: Implement the onRefresh function
   const onRefetch = () => {
     // Logic to refresh the transaction history
     console.log("Refreshing transaction history...");
   };
+
+  if (isLoading) {
+    return <TransactionLoadingSkeleton />;
+  }
+
+  if (isError || !data) {
+    return <TransactionErrorState />;
+  }
 
   return (
     <>
@@ -25,7 +37,7 @@ const TransactionHistory = () => {
         }}
       />
       <ScrollView refreshControl={<ListRefreshControl onRefresh={onRefetch} />}>
-        <TransactionList transactions={mockTransactions} />
+        <TransactionList transactions={data} />
       </ScrollView>
     </>
   );
