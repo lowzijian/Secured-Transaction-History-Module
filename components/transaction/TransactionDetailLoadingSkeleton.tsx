ff294bc05085d5e24e8b69f02e7231a1cdfb6dd2
lowzijian <jalow@moneylion.com>
2025-05-11 14:35:42 +0800

    feat: create detail loading skeleton

---
+++ components/transaction/TransactionDetailLoadingSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const TransactionDetailLoadingSkeleton = () => {
+  return (
+    <View>
+      <Text>Loading Transaction Detail ...</Text>
+    </View>
+  );
+};
+
+export default TransactionDetailLoadingSkeleton;
