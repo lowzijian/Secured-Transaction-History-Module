8c366c91280b35ff5bc74b13e0b18c7f90022d6a
lowzijian <jalow@moneylion.com>
2025-05-11 18:32:40 +0800

    feat: create auth service

---
+++ services/auth.service.ts
@@ -0,0 +1,29 @@
+import * as LocalAuthentication from "expo-local-authentication";
+
+const AuthService = {
+  async isBiometricSupported(): Promise<boolean> {
+    // Determine whether a face or fingerprint scanner is available on the device.
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    // Determine whether the device has saved fingerprints or facial data to use for authentication.
+    const enrolled = await LocalAuthentication.isEnrolledAsync();
+    return compatible && enrolled;
+  },
+
+  async authenticate(): Promise<boolean> {
+    const result = await LocalAuthentication.authenticateAsync({
+      promptMessage: "Authenticate",
+      fallbackLabel: "Use Passcode",
+      cancelLabel: "Cancel",
+    });
+    return result.success;
+  },
+
+  async getSupportedBiometricTypes(): Promise<
+    LocalAuthentication.AuthenticationType[]
+  > {
+    const types = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    return types;
+  },
+};
+
+export default AuthService;
