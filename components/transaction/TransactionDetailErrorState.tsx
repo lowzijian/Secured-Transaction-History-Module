3c700fcce9f71fe4f0d146036f2d0f91ebc9f069
lowzijian <jalow@moneylion.com>
2025-05-11 14:35:50 +0800

    feat: create detail error state

---
+++ components/transaction/TransactionDetailErrorState.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const TransactionDetailErrorState = () => {
+  return (
+    <View>
+      <Text>Something went wrong</Text>
+      <Text>Please try again later</Text>
+    </View>
+  );
+};
+
+export default TransactionDetailErrorState;
