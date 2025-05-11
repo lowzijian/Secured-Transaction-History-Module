703d970e05ad81f5b333743d0bb0b5eec2c8c9c6
lowzijian <jalow@moneylion.com>
2025-05-11 19:05:57 +0800

    feat:  add biometrics compatibility check on app start

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,8 +1,16 @@
-import AuthService from '@/services/auth.service';
-import { useState } from 'react';
+import AuthService from "@/services/auth.service";
+import { useEffect, useState } from "react";
 
 export const useAuth = () => {
+  const [isBiometricSupported, setIsBiometricSupported] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const isCompatible = await AuthService.isBiometricSupported();
+      setIsBiometricSupported(isCompatible);
+    })();
+  }, []);
 
   const authenticate = async () => {
     const success = await AuthService.authenticate();
@@ -13,5 +21,6 @@ export const useAuth = () => {
   return {
     isAuthenticated,
     authenticate,
+    isBiometricSupported,
   };
 };
