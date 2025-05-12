dc759660ec55b2128e7aba24bd70e57184421405
lowzijian <jalow@moneylion.com>
2025-05-12 22:05:38 +0800

    view: restyle transaction error screen

--- components/transaction/TransactionErrorState.tsx
+++ components/transaction/TransactionErrorState.tsx
@@ -1,12 +1,54 @@
-import { Text, View } from "react-native";
+import { COLORS, FONT_WEIGHT, SPACING } from "@/constants/theme";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Icon from "../Icon";
+import { FC } from "react";
 
-const TransactionErrorState = () => {
+interface TransactionErrorStateProps {
+  onRetry: VoidFunction;
+}
+
+const TransactionErrorState: FC<TransactionErrorStateProps> = (props) => {
+  const { onRetry } = props;
+
   return (
-    <View>
-      <Text>Something went wrong</Text>
-      <Text>Please try again later</Text>
+    <View style={styles.container}>
+      <Icon
+        name="robot-dead-outline"
+        size={100}
+        color={COLORS["primary"]}
+        style={styles.logo}
+      />
+      <Text style={styles.label}>Something went wrong</Text>
+      <Text style={styles.caption}>
+        Bzzt... initializing protocol sequence 9X-Δ. Zrrrp! Memory core
+        overloaded with snack data.
+      </Text>
+      <Button title="Retry" onPress={onRetry} color={COLORS["primary"]} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS["background-white"],
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: SPACING.S_4,
+  },
+  logo: {
+    marginBottom: SPACING.S_2,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: FONT_WEIGHT.BOLD,
+    marginBottom: SPACING.S_1,
+  },
+  caption: {
+    fontSize: 14,
+    color: COLORS["content-secondary"],
+    textAlign: "center",
+  },
+});
 
 export default TransactionErrorState;
