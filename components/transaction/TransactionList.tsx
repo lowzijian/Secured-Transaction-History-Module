9b64d1caccdb654bbc1ac478f10e1f5ce8c67b8c
lowzijian <jalow@moneylion.com>
2025-05-12 17:23:43 +0800

    view: restyle transaction list

--- components/transaction/TransactionList.tsx
+++ components/transaction/TransactionList.tsx
@@ -1,7 +1,5 @@
-import { COLORS, SPACING } from "@/constants/theme";
 import { Transaction } from "@/models/transaction.model";
 import { FC } from "react";
-import { StyleSheet, View } from "react-native";
 import TransactionItem from "./TransactionItem";
 
 interface TransactionListProps {
@@ -12,24 +10,13 @@ interface TransactionListProps {
 const TransactionList: FC<TransactionListProps> = (props) => {
   const { transactions, isAmountMasked } = props;
 
-  return (
-    <View style={styles.container}>
-      {transactions.map((transaction) => (
-        <TransactionItem
-          key={transaction.id}
-          {...transaction}
-          isAmountMasked={isAmountMasked}
-        />
-      ))}
-    </View>
-  );
+  return transactions.map((transaction) => (
+    <TransactionItem
+      key={transaction.id}
+      {...transaction}
+      isAmountMasked={isAmountMasked}
+    />
+  ));
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: SPACING.S_1,
-    backgroundColor: COLORS["background-white"],
-  },
-});
 
 export default TransactionList;
