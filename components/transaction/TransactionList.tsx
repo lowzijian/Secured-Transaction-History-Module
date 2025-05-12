c7ea6821eb7c3810c1f17527a7b221f373d91c3c
lowzijian <jalow@moneylion.com>
2025-05-12 18:08:16 +0800

    feat: adopt groupedTransactionByMonth util function

--- components/transaction/TransactionList.tsx
+++ components/transaction/TransactionList.tsx
@@ -1,6 +1,13 @@
 import { Transaction } from "@/models/transaction.model";
 import { FC } from "react";
 import TransactionItem from "./TransactionItem";
+import {
+  formatTransactionAmount,
+  groupedTransactionByMonth,
+  maskTransactionAmount,
+} from "@/utils/transaction.utils";
+import { StyleProp, StyleSheet, Text, TextStyle, View } from "react-native";
+import { COLORS, SPACING } from "@/constants/theme";
 
 interface TransactionListProps {
   transactions: Transaction[];
@@ -10,13 +17,54 @@ interface TransactionListProps {
 const TransactionList: FC<TransactionListProps> = (props) => {
   const { transactions, isAmountMasked } = props;
 
-  return transactions.map((transaction) => (
-    <TransactionItem
-      key={transaction.id}
-      {...transaction}
-      isAmountMasked={isAmountMasked}
-    />
-  ));
+  const groupedTransaction = groupedTransactionByMonth(transactions);
+
+  return Object.entries(groupedTransaction).map(
+    ([month, { total, transactions }]) => {
+      const totalAmountStyle: StyleProp<TextStyle> = {
+        color: total > 0 ? COLORS["content-positive"] : COLORS["text-primary"],
+      };
+
+      return (
+        <View key={month} style={styles.container}>
+          <Text style={styles.label}>{month.toUpperCase()}</Text>
+          {transactions.map((transaction) => (
+            <TransactionItem
+              key={transaction.id}
+              {...transaction}
+              isAmountMasked={isAmountMasked}
+            />
+          ))}
+          <View style={styles.footer}>
+            <Text style={styles.caption}>Total</Text>
+            <Text style={totalAmountStyle}>
+              {isAmountMasked
+                ? maskTransactionAmount(total)
+                : formatTransactionAmount(total)}
+            </Text>
+          </View>
+        </View>
+      );
+    }
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: SPACING.S_2,
+  },
+  label: {
+    fontSize: 18,
+    color: COLORS["content-secondary"],
+  },
+  footer: {
+    flexDirection: "row",
+    gap: SPACING.S_1,
+    marginLeft: "auto",
+  },
+  caption: {
+    color: COLORS["content-secondary"],
+  },
+});
 
 export default TransactionList;
