e490792af0622b84106da869b31905a851377e6f
lowzijian <jalow@moneylion.com>
2025-05-12 22:15:19 +0800

    view: add temp loading skeleton styling

--- components/transaction/TransactionDetailLoadingSkeleton.tsx
+++ components/transaction/TransactionDetailLoadingSkeleton.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from "react-native";
+import { COLORS, SPACING } from "@/constants/theme";
+import { StyleSheet, Text, View } from "react-native";
 
 const TransactionDetailLoadingSkeleton = () => {
   return (
-    <View>
-      <Text>Loading Transaction Detail ...</Text>
+    <View style={styles.container}>
+      <Text>[TODO] Loading Skeleton for Detail Screen ...</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: SPACING.S_2,
+    backgroundColor: COLORS["background-white"],
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default TransactionDetailLoadingSkeleton;
--- components/transaction/TransactionLoadingSkeleton.tsx
+++ components/transaction/TransactionLoadingSkeleton.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { COLORS, SPACING } from "@/constants/theme";
+import { StyleSheet, Text, View } from "react-native";
 
 const TransactionLoadingSkeleton = () => {
   return (
-    <View>
-      <Text>Loading ...</Text>
+    <View style={styles.container}>
+      <Text>[TODO] Loading Skeleton for History Screen ...</Text>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    padding: SPACING.S_2,
+    backgroundColor: COLORS["background-white"],
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default TransactionLoadingSkeleton;
