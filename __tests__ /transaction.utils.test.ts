7a6a7ea5d88635a76dd0096f496b993a8de80041
lowzijian <jalow@moneylion.com>
2025-05-13 21:31:19 +0800

    test: update groupedTransactionByMonth test

--- __tests__ /transaction.utils.test.ts
+++ __tests__ /transaction.utils.test.ts
@@ -47,59 +47,102 @@ describe("maskTransactionAmount", () => {
 
 // groupedTransactionByMonth
 describe("groupedTransactionByMonth", () => {
-  it("groups transactions by month with correct totals", () => {
-    const mockTransactions = [
-      {
-        id: "1",
-        amount: -100,
-        date: "2025-05-01T10:00:00Z",
-      },
-      {
-        id: "2",
-        amount: -200,
-        date: "2025-05-12T15:00:00Z",
-      },
-      {
-        id: "3",
-        amount: 1000,
-        date: "2025-04-15T08:00:00Z",
-      },
-    ] as Transaction[];
+  const mockTransaction2022Jan1 = {
+    date: "2022-01-01T00:00:00Z",
+    amount: -1,
+  } as Transaction;
+  const mockTransaction2022Jan2 = {
+    date: "2022-01-02T00:00:00Z",
+    amount: -2,
+  } as Transaction;
+  const mockTransaction2022Feb = {
+    date: "2022-02-01T00:00:00Z",
+    amount: -3,
+  } as Transaction;
+  const mockTransaction2022Jun = {
+    date: "2022-06-01T00:00:00Z",
+    amount: -4,
+  } as Transaction;
+  const mockTransaction2023Jan = {
+    date: "2023-01-01T00:00:00Z",
+    amount: -5,
+  } as Transaction;
+  const mockTransaction2024Jan = {
+    date: "2024-01-01T00:00:00Z",
+    amount: -6,
+  } as Transaction;
 
-    const expectedResult = {
-      "May 2025": {
-        total: -300,
-        transactions: [
-          {
-            id: "1",
-            amount: -100,
-            date: "2025-05-01T10:00:00Z",
-          },
-          {
-            id: "2",
-            amount: -200,
-            date: "2025-05-12T15:00:00Z",
-          },
-        ],
-      },
-      "April 2025": {
-        total: 1000,
-        transactions: [
-          {
-            id: "3",
-            amount: 1000,
-            date: "2025-04-15T08:00:00Z",
-          },
-        ],
-      },
-    };
+  test("empty transactions", () => {
+    const transactions: Transaction[] = [];
+    const result = groupedTransactionByMonth(transactions);
+    expect(result).toEqual([]);
+  });
 
-    const result = groupedTransactionByMonth(mockTransactions as any);
-
+  it("transactions within the same month", () => {
+    const result = groupedTransactionByMonth([
+      mockTransaction2022Jan2,
+      mockTransaction2022Jan1,
+    ]);
+    const expectedResult: ReturnType<typeof groupedTransactionByMonth> = [
+      {
+        title: "January 2022",
+        data: [mockTransaction2022Jan2, mockTransaction2022Jan1],
+        total: -3,
+      },
+    ];
     expect(result).toEqual(expectedResult);
   });
 
-  it("returns empty object when input is empty", () => {
-    expect(groupedTransactionByMonth([])).toEqual({});
+  test("transactions spanning various months but occurring within the same year", () => {
+    const result = groupedTransactionByMonth([
+      mockTransaction2022Jun,
+      mockTransaction2022Feb,
+      mockTransaction2022Jan2,
+    ]);
+    const expectedResult: ReturnType<typeof groupedTransactionByMonth> = [
+      {
+        title: "June 2022",
+        data: [mockTransaction2022Jun],
+        total: -4,
+      },
+      {
+        title: "February 2022",
+        data: [mockTransaction2022Feb],
+        total: -3,
+      },
+      {
+        title: "January 2022",
+        data: [mockTransaction2022Jan2],
+        total: -2,
+      },
+    ];
+    expect(result).toEqual(expectedResult);
+  });
+
+  test("transactions occur in different years", () => {
+    const result = groupedTransactionByMonth([
+      mockTransaction2024Jan,
+      mockTransaction2023Jan,
+      mockTransaction2022Jan1,
+    ]);
+
+    const expectedResult: ReturnType<typeof groupedTransactionByMonth> = [
+      {
+        title: "January 2024",
+        data: [mockTransaction2024Jan],
+        total: -6,
+      },
+      {
+        title: "January 2023",
+        data: [mockTransaction2023Jan],
+        total: -5,
+      },
+      {
+        title: "January 2022",
+        data: [mockTransaction2022Jan1],
+        total: -1,
+      },
+    ];
+    expect(result).toEqual(expectedResult);
   });
 });
