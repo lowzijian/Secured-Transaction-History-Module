96c3430750726b4563bb2b2e41fbd332b108d6ae
lowzijian <jalow@moneylion.com>
2025-05-17 21:25:21 +0800

    feat: add transaction history screen loading skeleton

--- components/transaction/TransactionLoadingSkeleton.tsx
+++ components/transaction/TransactionLoadingSkeleton.tsx
@@ -1,10 +1,32 @@
 import { COLORS, SPACING } from "@/constants/theme";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import ShimmerPlaceholder from "../ShimmerPlaceholder";
+
+const SKELETON_COUNT = 10;
 
 const TransactionLoadingSkeleton = () => {
+  const renderSkeletonItem = (index: number) => (
+    <View key={index} style={styles.itemContainer}>
+      <ShimmerPlaceholder width={40} height={40} borderRadius={20} />
+      <View style={styles.content}>
+        <ShimmerPlaceholder width={"100%"} height={20} borderRadius={16} />
+        <ShimmerPlaceholder width={"40%"} height={20} borderRadius={16} />
+      </View>
+      <ShimmerPlaceholder width={60} height={20} borderRadius={16} />
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>[TODO] Loading Skeleton for History Screen ...</Text>
+      <ShimmerPlaceholder
+        width={100}
+        height={20}
+        borderRadius={16}
+        style={styles.titleSkeleton}
+      />
+      {Array.from({ length: SKELETON_COUNT }, (_, index) =>
+        renderSkeletonItem(index)
+      )}
     </View>
   );
 };
@@ -14,8 +36,18 @@ const styles = StyleSheet.create({
     padding: SPACING.S_2,
     backgroundColor: COLORS["background-white"],
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  titleSkeleton: {
+    marginBottom: SPACING.S_1,
+  },
+  itemContainer: {
+    flexDirection: "row",
+    gap: SPACING.S_2,
+    paddingVertical: SPACING.S_2,
+  },
+  content: {
+    flexGrow: 1,
+    gap: SPACING.S_1,
   },
 });
 
