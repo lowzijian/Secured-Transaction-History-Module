9f1b6218c97cf6ab40844af2d33b2d24a41a89b2
lowzijian <jalow@moneylion.com>
2025-05-18 17:26:26 +0800

    feat: add transaction detail loading skeleton

--- components/transaction/detail/TransactionDetailLoadingSkeleton.tsx
+++ components/transaction/detail/TransactionDetailLoadingSkeleton.tsx
@@ -1,10 +1,43 @@
+import ShimmerPlaceholder from "@/components/ShimmerPlaceholder";
 import { COLORS, SPACING } from "@/constants/theme";
-import { StyleSheet, Text, View } from "react-native";
+import { Fragment } from "react";
+import { StyleSheet, View } from "react-native";
 
 const TransactionDetailLoadingSkeleton = () => {
+  const renderHeaderSkeleton = () => (
+    <View style={styles.headerContainer}>
+      <ShimmerPlaceholder height={80} width={80} borderRadius={40} />
+      <ShimmerPlaceholder height={32} width={160} borderRadius={16} />
+      <ShimmerPlaceholder height={16} width={120} borderRadius={16} />
+    </View>
+  );
+
+  const renderItemRowSkeleton = (index: number) => (
+    <View key={index} style={styles.itemSkeleton}>
+      <ShimmerPlaceholder height={24} width={150} borderRadius={16} />
+      <ShimmerPlaceholder height={24} width={60} borderRadius={16} />
+    </View>
+  );
+
+  const renderGroupSkeletons = (index: number) => (
+    <Fragment key={index}>
+      <View style={styles.group} key={index}>
+        {Array.from({ length: 3 }, (_, index) => renderItemRowSkeleton(index))}
+      </View>
+      {index < 2 && <View style={styles.divider} />}
+    </Fragment>
+  );
+
+  const renderBodySkeleton = () => (
+    <View style={styles.bodyContainer}>
+      {Array.from({ length: 3 }, (_, index) => renderGroupSkeletons(index))}
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>[TODO] Loading Skeleton for Detail Screen ...</Text>
+      {renderHeaderSkeleton()}
+      {renderBodySkeleton()}
     </View>
   );
 };
@@ -15,7 +48,30 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS["background-white"],
     flex: 1,
     alignItems: "center",
+  },
+  headerContainer: {
+    paddingVertical: SPACING.S_4,
+    alignItems: "center",
     justifyContent: "center",
+    gap: SPACING.S_1,
+  },
+  bodyContainer: {
+    paddingVertical: SPACING.S_2,
+    paddingHorizontal: SPACING.S_3,
+    width: "100%",
+  },
+  itemSkeleton: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  group: {
+    gap: SPACING.S_2,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: COLORS["border-primary"],
+    marginVertical: SPACING.S_3,
   },
 });
 
